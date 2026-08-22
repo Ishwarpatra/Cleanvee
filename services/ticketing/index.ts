bc876d0b3eb672574fcdd06c9f9862b608c8a771
Ishwarpatra <1.28956752e+08+Ishwarpatra@users.noreply.github.com>
2026-08-22 13:57:13 +0000

    chore: retire legacy Firebase implementation

--- services/ticketing/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Ticketing Module Index
- * Re-exports all ticketing-related services
- */
-
-export * from './types';
-export { ServiceNowConnector, getServiceNowConnector } from './serviceNowConnector';
-export { JiraConnector, getJiraConnector } from './jiraConnector';
